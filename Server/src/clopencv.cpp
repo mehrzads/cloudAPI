d6dd48b563ea1bc1072dcb90acdc03ba907c5f47
Mehrzad Samadi <mehrzadsamadi@gmail.com>
2015-04-15 15:26:25 -0400

    Make

--- Server/src/clopencv.cpp
+++ Server/src/clopencv.cpp
@@ -31,10 +31,6 @@ cloudError_t cloudFaceTrain(int rows, int cols,
      labelsVec.push_back(labels[i]);
   }
 
-  for (int i = 0; i < 10; i++)
-	printf("%d\t%d\t%d\t%d\t%d\n",labels[i], images[i], i, imagesMat.data[i], imagesVec[0].data[i]);
-
-  printf("labels.size %d\t images total %d\t images elem %d\n", labelsVec.size(),  imagesMat.total() ,imagesMat.elemSize());
 
   Ptr<FaceRecognizer> model = createEigenFaceRecognizer();
 //  vector<int> labelsVec(labels, labels + rows *sizeof(int));
@@ -42,24 +38,16 @@ cloudError_t cloudFaceTrain(int rows, int cols,
   // Here is how to get the eigenvalues of this Eigenfaces model:
   Mat eigenValuesMat = model->getMat("eigenvalues");
   memcpy(eigenValues, reinterpret_cast<double *>(eigenValuesMat.data), eigenValuesMat.total() * eigenValuesMat.elemSize());
-  printf("EigenValues %dX%d %d %d\n", eigenValuesMat.rows, eigenValuesMat.cols, eigenValuesMat.elemSize(), eigenValuesMat.type()); 
   // And we can do the same to display the Eigenvectors (read Eigenfaces):
   Mat eigenVectorsMat = model->getMat("eigenvectors");
   memcpy(eigenVectors, reinterpret_cast<double *>(eigenVectorsMat.data), eigenVectorsMat.total() * eigenVectorsMat.elemSize());
-  printf("EigenVectors %dX%d %d %d\n", eigenVectorsMat.rows, eigenVectorsMat.cols, eigenVectorsMat.elemSize(), eigenVectorsMat.type()); 
   // Get the sample mean from the training data
   Mat meanMat = model->getMat("mean");
   memcpy(mean, reinterpret_cast<double *>(meanMat.data), meanMat.total() * meanMat.elemSize());
-  printf("mean %dX%d %d %d\n", meanMat.rows, meanMat.cols, meanMat.elemSize(), meanMat.type());
 
   vector<Mat> projectionsVec = model->getMatVector("projections");
-  printf("Projections %d %dX%d %d %d\n", projectionsVec.size(), projectionsVec[0].rows, projectionsVec[0].cols, projectionsVec[0].elemSize(), projectionsVec[0].type());
-
   Mat projectionsMat = model->asFaceRowMatrix(projectionsVec, CV_64FC1);
   memcpy(projections, reinterpret_cast<double *>(projectionsMat.data), projectionsMat.total() * projectionsMat.elemSize());
-  
-
-
   printf("cloud Face Train is called\n");
   return CloudSuccess; 
 }
